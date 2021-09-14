a5b517d9a7f1aa537dda13a3b0d7a5bbdeb5d28f
Warturbed <alex.ivanov99@inbox.ru>
2021-09-14 22:24:15 +0700

    add data

--- src/data/list.js
+++ src/data/list.js
@@ -5,7 +5,128 @@ export default {
     folders: [
       {
         name: "icons",
-        folders:[],
+        folders:[
+          {
+            name: "cows",
+            folders:[],
+            files: [
+              {
+                name: "big-brown-cow.png",
+                type: "image/png",
+                length: "1024"
+              },
+              {
+                name: "inside-cow.png",
+                type: "image/png",
+                length: "1024"
+              },
+              {
+                name: "more-cows.png",
+                type: "image/png",
+                length: "1024"
+              },
+              {
+                name: "cows-in-black.png",
+                type: "image/png",
+                length: "1024"
+              }
+            ]
+          },
+          {
+            name: "rabits",
+            folders:[],
+            files: [
+              {
+                name: "small-rabit.png",
+                type: "image/png",
+                length: "1024"
+              },
+              {
+                name: "cook-rabit.png",
+                type: "image/png",
+                length: "1024"
+              },
+              {
+                name: "love-rabits.png",
+                type: "image/png",
+                length: "1024"
+              }
+            ]
+          },
+          {
+            name: "foxes",
+            folders:[],
+            files: [
+              {
+                name: "cute-foxes.png",
+                type: "image/png",
+                length: "1024"
+              },
+              {
+                name: "foxes-are-the-best.png",
+                type: "image/png",
+                length: "1024"
+              },
+              {
+                name: "foxes-forever.png",
+                type: "image/png",
+                length: "1024"
+              },
+              {
+                name: "me-and-foxes.png",
+                type: "image/png",
+                length: "1024"
+              },
+              {
+                name: "kind-of-foxes.png",
+                type: "image/png",
+                length: "1024"
+              },
+              {
+                name: "kiss-foxes.png",
+                type: "image/png",
+                length: "1024"
+              }
+            ]
+          },
+          {
+            name: "deer",
+            folders:[],
+            files: [
+              {
+                name: "deer-in-forest.jpeg",
+                type: "image/jpeg",
+                length: "1024"
+              },
+              {
+                name: "big-deer.jpeg",
+                type: "image/jpeg",
+                length: "1024"
+              },
+              {
+                name: "love-deer.jpeg",
+                type: "image/jpeg",
+                length: "1024"
+              }
+            ]
+          },
+          {
+            name: "elefants",
+            folders:[],
+            files: [
+              {
+                name: "huge-elefants.jpeg",
+                type: "image/jpeg",
+                length: "1024"
+              },
+              {
+                name: "elefants-in-africa.jpeg",
+                type: "image/jpeg",
+                length: "1024"
+              }
+            ]
+          }
+        ],
         files: [
           {
             name: "close.svg",
